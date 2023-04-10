7006b156c7c6350654be02beb906ecb8ce6339ff
Wesley Chou <wesleychou9851@gmail.com>
2023-04-09 19:02:40 -0700

    class to try out diff models

---
+++ my_models.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
+from keras.regularizers import l2
+
+class MyModels():
+    
+    def __init__(self):
+        self.models = {}
+        self.current_model = None
+    
+    
+    def add_model(self, model_name, model):
+        """
+        Add a model.
+        """
+        self.models[model_name] = model
+    
+    
+    def del_model(self, model_name):
+        """
+        Delete an existing model.
+        """
+        del self.models[model_name]
+    
+    
+    def set_model(self, model_name):
+        """
+        Set current model to an existing model.
+        """    
+        if model_name not in self.models:
+            raise ValueError("Invalid model name.")
+        
+        self.current_model = self.models[model_name]
