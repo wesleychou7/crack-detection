90fb3e0605a1fed1340059c08d657b992e31374f
Wesley Chou <wesleychou9851@gmail.com>
2023-04-14 22:24:45 -0700

    new functions

--- my_models.py
+++ my_models.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
@@ -16,6 +17,14 @@ class MyModels():
         Prints the list of model names.
         """
         print(list(self.models.keys()))
+        
+    
+    def get_model(self, model_name):
+        """
+        Returns a copy of the specified model.
+        """
+        return self.models[model_name]
+    
     
     def add_model(self, model_name, model):
         """
@@ -29,6 +38,13 @@ class MyModels():
         Delete an existing model.
         """
         del self.models[model_name]
+        
+    
+    def del_all_models(self):
+        """
+        Deletes all models.
+        """
+        self.models = {}
     
     
     def set_model(self, model_name):
@@ -39,3 +55,33 @@ class MyModels():
             raise ValueError("Invalid model name.")
         
         self.current_model = self.models[model_name]
+    
+    
+    def run_and_evaluate(self, X_train, y_train, X_cv, y_cv, learning_rate, epochs):
+        """
+        Run all the models and show its accuracy plot
+        """
+        fig, axes = plt.subplots(ncols=len(self.models), figsize=(12,3), sharey=True)
+        fig.supylabel('Accuracy')
+        fig.supxlabel('Epoch')
+                
+        for i, (model_name, model) in enumerate(self.models.items()):
+            
+            model.compile(
+                loss = tf.keras.losses.BinaryCrossentropy(),
+                optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                metrics=['accuracy']
+            )
+            
+            history = model.fit(X_train, y_train, epochs=epochs, 
+                                validation_data=(X_cv, y_cv), verbose=0)
+            
+            axes[i].set_title(model_name)
+            axes[i].plot(history.history['accuracy'], label='accuracy')
+            axes[i].plot(history.history['val_accuracy'], label = 'val_accuracy')
+            
+            cv_loss, cv_acc = model.evaluate(X_cv, y_cv, verbose=0)
+            print(f"{model_name}:")
+            print(f"\tloss = {cv_loss}\n\taccuracy = {cv_acc}")
+            
+            
