0b0f0c78999dc1e11389c98a482e1d27f55d7ae5
Wesley Chou <wesleychou9851@gmail.com>
2023-04-16 16:39:07 -0700

    removed unused imports and functions

--- myutils.py
+++ myutils.py
@@ -1,33 +1,5 @@
 import os
-import random
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
 from PIL import Image, ImageOps
-
-def create_model(input_shape):
-    
-    tf.random.set_seed(42)
-    model = Sequential(
-        [
-            Conv2D(16, (3,3), activation='relu', input_shape=input_shape),
-            MaxPooling2D((2,2)),
-            Conv2D(32, (3,3), activation='relu'),
-            MaxPooling2D((2,2)),
-            Conv2D(64, (3,3), activation='relu'),
-            MaxPooling2D((2,2)),
-            
-            Flatten(),
-            Dense(128, activation='relu'),
-            Dense(1, activation='sigmoid')
-            
-        ], name = "mymodel"
-    )
-    
-    return model
 
 
 def load_images(directory):
